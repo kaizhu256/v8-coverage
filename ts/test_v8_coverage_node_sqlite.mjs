f4ac8ad97ba613c8d32600b627188a866fb6e9df
kai zhu <kaizhu256@gmail.com>
2021-10-08 01:13:46 -0500

    log.test

--- ts/test_v8_coverage_node_sqlite.mjs
+++ ts/test_v8_coverage_node_sqlite.mjs
@@ -72,6 +72,7 @@ import {mergeProcessCovs} from "./lib/index.js";
     data1 = JSON.stringify(data1, undefined, 4) + "\n";
     await fs.promises.writeFile(".v8_coverage_node_sqlite_merged.json", data1);
     assert_or_throw(data1 === data2);
+    console.error("finished test_v8_coverage_node_sqlite.mjs");
 
     // coverage-hack
     noop();
