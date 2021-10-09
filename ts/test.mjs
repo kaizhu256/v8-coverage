6a464cadc591f771ee55d41921698f13d6d8aaea
kai zhu <kaizhu256@gmail.com>
2021-10-09 14:13:57 -0500

    test.mjs

--- ts/test.mjs
+++ ts/test.mjs
@@ -1,94 +1,36 @@
 /*jslint node*/
-import moduleChai from "chai";
 import moduleFs from "fs";
 import moduleSysPath from "path";
 import modulePath from "path";
 import moduleUrl from "url";
 import coverageMerge from "./merge.mjs";
 
-let {
-    coverageFunctionListMerge,
-    coverageProcessListMerge,
-    coverageScriptListMerge
-} = coverageMerge;
+(function () {
+    let {
+        coverageFunctionListMerge,
+        coverageProcessListMerge,
+        coverageScriptListMerge
+    } = coverageMerge;
 
-//!! let testDescribeList = [];
-let testItList;
+    let testItList;
 
-async function describe(description, testDescribe) {
-    testItList = [];
-    testDescribe();
-    testItList = await Promise.all(testItList);
-    throw description;
-    console.error(description);
-    testItList.forEach(function ([
-        err, description
-    ]) {
-        console.error(
-            (
-                err
-                ? "\u2714 "
-                : "\u2718 "
-            ) + description
-        );
-        if (err) {
-            console.error(err);
-        }
-    });
-}
+    // init debugInline
+    let debugInline = (function () {
+        let console_error = function () {
+            return;
+        };
+        return function (...argv) {
 
-function it(description, testIt) {
-    testItList.push(new Promise(async function (resolve) {
-        let err;
-        try {
-            await testIt();
-        } catch (errCaught) {
-            err = errCaught;
-        }
-        return [
-            err, description
-        ];
-    }));
-}
+    // This function will both print <argv> to stderr and return <argv>[0].
 
-/**
- * Generate a Mocha test suite for the provided
- * implementation of `v8-coverage-tools`.
- */
-describe("merge", function () {
-    it("accepts empty arrays for `coverageProcessListMerge`", function () {
-        const inputs = [];
-        const expected = { result: [] };
-        const actual = coverageProcessListMerge(inputs);
-        moduleChai.assert.deepEqual(actual, expected);
-    });
-    it("accepts empty arrays for `coverageScriptListMerge`", function () {
-        const inputs = [];
-        const expected = undefined;
-        const actual = coverageScriptListMerge(inputs);
-        moduleChai.assert.deepEqual(actual, expected);
-    });
-    it("accepts empty arrays for `coverageFunctionListMerge`", function () {
-        const inputs = [];
-        const expected = undefined;
-        const actual = coverageFunctionListMerge(inputs);
-        moduleChai.assert.deepEqual(actual, expected);
-    });
-});
-const REPO_ROOT = modulePath.join(moduleUrl.fileURLToPath(import.meta.url), "..", "..", "..");
-const MERGE_TESTS_DIR = modulePath.join(REPO_ROOT, "tests", "merge");
-const MERGE_TIMEOUT = 30000; // 30sec
-// `BLACKLIST` can be used to forcefully skip some tests.
-const BLACKLIST = new Set([
-    ...["node-10.11.0", "npm-6.4.1", "yargs-12.0.2"],
-    // ...(process.env.CI === "true" ? ["node-10.11.0", "npm-6.4.1"] : []),
-]);
-// `WHITELIST` can be used to only enable a few tests.
-const WHITELIST = new Set([
-// "simple",
-]);
+            console_error("\n\ndebugInline");
+            console_error(...argv);
+            console_error("\n");
+            console_error = console.error;
+            return argv[0];
+        };
+    }());
 
-describe("merge", function () {
     function assertJsonEqual(aa, bb) {
 
 // this function will assert JSON.stringify(<aa>) === JSON.stringify(<bb>)
@@ -146,12 +88,94 @@ describe("merge", function () {
         return sorted;
     }
 
-    it("accepts arrays with a single item for `coverageProcessListMerge`", function () {
-        assertJsonEqual(coverageProcessListMerge([
-            {
+    async function describe(description, testDescribe) {
+        let resultItList;
+        testItList = [];
+        testDescribe();
+        resultItList = await Promise.all(testItList);
+        console.error(description);
+        resultItList.forEach(function ([
+            err, description
+        ]) {
+            console.error(
+                (
+                    err
+                    ? "\u2718 "
+                    : "\u2714 "
+                ) + description
+            );
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
+
+    function it(description, testIt) {
+        testItList.push(new Promise(async function (resolve) {
+            let err;
+            try {
+                await testIt();
+            } catch (errCaught) {
+                err = errCaught;
+            }
+            resolve([
+                err, description
+            ]);
+        }));
+    }
+
+    /**
+     * Generate a Mocha test suite for the provided
+     * implementation of `v8-coverage-tools`.
+     */
+    describe("merge", function () {
+        it("accepts empty arrays for `coverageProcessListMerge`", function () {
+            const inputs = [];
+            const expected = { result: [] };
+            const actual = coverageProcessListMerge(inputs);
+            assertJsonEqual(actual, expected);
+        });
+        it("accepts empty arrays for `coverageScriptListMerge`", function () {
+            const inputs = [];
+            const expected = undefined;
+            const actual = coverageScriptListMerge(inputs);
+            assertJsonEqual(actual, expected);
+        });
+        it("accepts empty arrays for `coverageFunctionListMerge`", function () {
+            const inputs = [];
+            const expected = undefined;
+            const actual = coverageFunctionListMerge(inputs);
+            assertJsonEqual(actual, expected);
+        });
+    });
+
+    describe("merge", function () {
+
+        it("accepts arrays with a single item for `coverageProcessListMerge`", function () {
+            assertJsonEqual(coverageProcessListMerge([
+                {
+                    result: [
+                        {
+                            scriptId: "123",
+                            moduleUrl: "/lib.js",
+                            functions: [
+                                {
+                                    functionName: "test",
+                                    isBlockCoverage: true,
+                                    ranges: [
+                                        { startOffset: 0, endOffset: 4, count: 2 },
+                                        { startOffset: 1, endOffset: 2, count: 1 },
+                                        { startOffset: 2, endOffset: 3, count: 1 },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ]), {
                 result: [
                     {
-                        scriptId: "123",
+                        scriptId: "0",
                         moduleUrl: "/lib.js",
                         functions: [
                             {
@@ -159,18 +183,18 @@ describe("merge", function () {
                                 isBlockCoverage: true,
                                 ranges: [
                                     { startOffset: 0, endOffset: 4, count: 2 },
-                                    { startOffset: 1, endOffset: 2, count: 1 },
-                                    { startOffset: 2, endOffset: 3, count: 1 },
+                                    { startOffset: 1, endOffset: 3, count: 1 },
                                 ],
                             },
                         ],
                     },
                 ],
-            },
-        ]), {
-            result: [
+            });
+        });
+        it("accepts arrays with a single item for `coverageScriptListMerge`", function () {
+            assertJsonEqual(coverageScriptListMerge([
                 {
-                    scriptId: "0",
+                    scriptId: "123",
                     moduleUrl: "/lib.js",
                     functions: [
                         {
@@ -178,17 +202,13 @@ describe("merge", function () {
                             isBlockCoverage: true,
                             ranges: [
                                 { startOffset: 0, endOffset: 4, count: 2 },
-                                { startOffset: 1, endOffset: 3, count: 1 },
+                                { startOffset: 1, endOffset: 2, count: 1 },
+                                { startOffset: 2, endOffset: 3, count: 1 },
                             ],
                         },
                     ],
                 },
-            ],
-        });
-    });
-    it("accepts arrays with a single item for `coverageScriptListMerge`", function () {
-        assertJsonEqual(coverageScriptListMerge([
-            {
+            ]), {
                 scriptId: "123",
                 moduleUrl: "/lib.js",
                 functions: [
@@ -197,65 +217,53 @@ describe("merge", function () {
                         isBlockCoverage: true,
                         ranges: [
                             { startOffset: 0, endOffset: 4, count: 2 },
-                            { startOffset: 1, endOffset: 2, count: 1 },
-                            { startOffset: 2, endOffset: 3, count: 1 },
+                            { startOffset: 1, endOffset: 3, count: 1 },
                         ],
                     },
                 ],
-            },
-        ]), {
-            scriptId: "123",
-            moduleUrl: "/lib.js",
-            functions: [
+            });
+        });
+        it("accepts arrays with a single item for `coverageFunctionListMerge`", function () {
+            assertJsonEqual(coverageFunctionListMerge([
                 {
                     functionName: "test",
                     isBlockCoverage: true,
                     ranges: [
                         { startOffset: 0, endOffset: 4, count: 2 },
-                        { startOffset: 1, endOffset: 3, count: 1 },
+                        { startOffset: 1, endOffset: 2, count: 1 },
+                        { startOffset: 2, endOffset: 3, count: 1 },
                     ],
                 },
-            ],
-        });
-    });
-    it("accepts arrays with a single item for `coverageFunctionListMerge`", function () {
-        assertJsonEqual(coverageFunctionListMerge([
-            {
+            ]), {
                 functionName: "test",
                 isBlockCoverage: true,
                 ranges: [
                     { startOffset: 0, endOffset: 4, count: 2 },
-                    { startOffset: 1, endOffset: 2, count: 1 },
-                    { startOffset: 2, endOffset: 3, count: 1 },
+                    { startOffset: 1, endOffset: 3, count: 1 },
                 ],
-            },
-        ]), {
-            functionName: "test",
-            isBlockCoverage: true,
-            ranges: [
-                { startOffset: 0, endOffset: 4, count: 2 },
-                { startOffset: 1, endOffset: 3, count: 1 },
-            ],
+            });
         });
+        Promise.all([
+            "test_merge_is_block_coverage_test.json",
+            "test_merge_issue_2_mixed_is_block_coverage_test.json",
+            //!! "test_merge_node_10_11_0_test.json",
+            "test_merge_node_10_internal_errors_one_of_test.json",
+            //!! "test_merge_npm_6_4_1_test.json",
+            "test_merge_reduced_test.json",
+            "test_merge_simple_test.json",
+            "test_merge_various_test.json"
+        ].map(async function (pathname) {
+            JSON.parse(
+                await moduleFs.promises.readFile(pathname, "utf8")
+            ).forEach(function ({
+                expected,
+                inputs
+            }) {
+                assertJsonEqual(coverageProcessListMerge(inputs), expected);
+            });
+        }));
     });
-    Promise.all([
-        "test_merge_is_block_coverage_test.json",
-        "test_merge_issue_2_mixed_is_block_coverage_test.json",
-        //!! "test_merge_node_10_11_0_test.json",
-        "test_merge_node_10_internal_errors_one_of_test.json",
-        //!! "test_merge_npm_6_4_1_test.json",
-        "test_merge_reduced_test.json",
-        "test_merge_simple_test.json",
-        "test_merge_various_test.json"
-    ].map(async function (pathname) {
-        JSON.parse(
-            await moduleFs.promises.readFile(pathname, "utf8")
-        ).forEach(function ({
-            expected,
-            inputs
-        }) {
-            assertJsonEqual(coverageProcessListMerge(inputs), expected);
-        });
-    }));
-});
-await import("./test_v8_coverage_node_sqlite.mjs");
+    //!! await import("./test_v8_coverage_node_sqlite.mjs");
+    // Coverage-hack.
+    debugInline();
+}());
